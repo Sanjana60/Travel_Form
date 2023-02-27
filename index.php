5cd23b4e00be7e600b2d3096afc84add9df72827
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-27 16:14:19 +0530

    database con

--- index.php
+++ index.php
@@ -13,6 +13,18 @@ if(!$con){
     die("connection to this database failed due to".mysqli_connect_error());
 }
 
-echo "Success connecting to be db";
+// echo "Success connecting to be db";
+
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$desc = $_POST['desc'];
+
+
+$sql = "INSERT INTO `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+
+echo $sql;
 
 ?>
