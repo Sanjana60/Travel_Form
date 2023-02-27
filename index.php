196844100b4ed6f85cbe06074322de72c57f5db2
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-02-27 16:21:04 +0530

    html+php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-
+if(isset($POST['name'])){
 $server = "localhost";
 $username = "root";
 $password = "";
@@ -23,8 +22,55 @@ $phone = $_POST['phone'];
 $desc = $_POST['desc'];
 
 
-$sql = "INSERT INTO `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+$sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
 
-echo $sql;
+// echo $sql;
 
+if($con->query($sql) == true){
+
+    echo "Success";
+}
+
+else{
+
+    echo "ERROR: $sql <br>  $con->error";
+}
+
+$con->close();
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Barlow:wght@200&family=Julee&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class = "bg" src="images.png" alt="Bennett University">
+    <div class="container">
+        <h1>Welcome to Bennett University US Trip form</h1>
+        <p>Enter your details and submit this form to confirm your participation in the trip.</p>
+        <p class="submitMsg">Thanks for filling out the form. We are happy to see you joining us for the US trip.</p>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+
+            <button class="btn">Submit</button>
+            
+        </form>
+    </div>
+    <script src="index.js"></script>
+    
+</body>
+</html>
